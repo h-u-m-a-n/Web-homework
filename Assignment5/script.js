f71aad207ca9e33975d8f180f4bf8fa61397bc6c
Sanzhar <190103205@stu.sdu.edu.kz>
2020-11-02 20:03:20 +0600

    finished fifth assignment

--- Assignment5/script.js
+++ Assignment5/script.js
@@ -1,12 +1,15 @@
+
 function enlarge(event, i) {
     document.body.style.overflow = 'hidden';
     const div = document.createElement('div');
     const img = document.createElement('img');
-    console.log(event);
+    img.classList.add('appearance');
+    img.style.touchAction = 'none';
     img.src = event.currentTarget.src;
     img.style.backgroundColor = "#fff";
+    div.style.touchAction = 'none';
     div.style.position = 'absolute';
-    div.style.top = scrollY+'px';
+    div.style.top = scrollY + 'px';
     div.style.left = '0';
     div.style.width = '100%';
     div.style.height = '100%';
@@ -15,31 +18,42 @@ function enlarge(event, i) {
     div.style.justifyContent = 'center';
     div.style.alignItems = 'center';
     div.appendChild(img);
+
+
+
     document.body.appendChild(div);
-    div.addEventListener('click', function(){
+    div.addEventListener('click', function () {
         document.body.style.overflow = 'auto';
         document.body.removeChild(div);
     });
 
-    document.addEventListener('keydown', function(event){
+    document.addEventListener('keydown', function (event) {
         const imgs = document.querySelectorAll('img');
-        if(event.key === 'ArrowRight'){
-            if(i+1 < imgs.length){
+        if (event.key === 'ArrowRight') {
+            if (i + 1 < imgs.length) {
+                div.removeChild(img);
                 img.src = imgs[++i].src;
+                img.classList.add('toRight');
+                div.appendChild(img);
+
             }
         }
-        else if(event.key === 'ArrowLeft'){
-            if(i-1 >= 0){
+        else if (event.key === 'ArrowLeft') {
+            if (i - 1 >= 0) {
+                div.removeChild(img);
                 img.src = imgs[--i].src;
+                img.classList.add('toLeft');
+                div.appendChild(img);
             }
         }
+
     })
 }
 
 
 const imgs = document.querySelectorAll('img');
-for(let i = 0; i < imgs.length; i++){
-    imgs[i].addEventListener('click', function(event){
+for (let i = 0; i < imgs.length; i++) {
+    imgs[i].addEventListener('click', function (event) {
         enlarge(event, i);
     });
-}
+}   
